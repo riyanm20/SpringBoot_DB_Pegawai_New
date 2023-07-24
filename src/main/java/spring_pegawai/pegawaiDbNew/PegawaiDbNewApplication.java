9e06485d30e47c87b2ca4c3eed52cfdb679b8787
Riyan Sudibio <riyanm20@gmail.com>
2023-07-24 10:30:04 +0700

    Config (application.properties)

---
+++ src/main/java/spring_pegawai/pegawaiDbNew/PegawaiDbNewApplication.java
@@ -0,0 +1,13 @@
+package spring_pegawai.pegawaiDbNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegawaiDbNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegawaiDbNewApplication.class, args);
+	}
+
+}
